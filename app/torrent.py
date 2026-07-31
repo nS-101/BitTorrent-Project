fab773a84dcb8471087a6760e78e3441aae0f531
Neil Singh <neilsingh.miami@gmail.com>
2026-07-30 20:25:07 -0400

    next stage of code refactoring done: with torrent.py, we bundle up the redundant code related to extracting info from the .torrent file by using a class and multiple methods

---
+++ app/torrent.py
@@ -0,0 +1,44 @@
+import hashlib 
+import bencode #import the file for its decoder methods
+
+
+class Torrent:
+    """represents a parsed .torrent file"""
+    
+    @classmethod
+    def fromFile(cls, torrentFile:str):
+        """Read a .torrent file off the disk and built a Torrent from it"""
+        with open(torrentFile, "rb") as file:
+            fileContents = file.read()
+            torrentData = bencode.decodeBencode(fileContents) #use bencode file to decode
+            announce = torrentData[b"announce"] 
+            infoDict = torrentData[b"info"] #get info dict from torrent file
+            return cls(announce, infoDict) #return these two data pieces so init can use them
+
+    def __init__(self, announce:bytes, infoDict:dict):
+        self.announce = announce
+        self.infoDict = infoDict
+        infoDict = bencode.encodeBencode(infoDict) #dict needs to be bencoded to be turned into a hashed dict
+        infoHash = hashlib.sha1(infoDict).digest() #convert infoDictionary into sha1 hash(not hex, just bytes)
+        self.infoHash = infoHash
+
+    @property
+    def length(self) -> int:
+        """get total file length in bytes form"""
+        length = self.infoDict[b"length"] #self.infoDict already represents the outer key of ["info"] so we just access the length from there
+        return length
+    
+    @property
+    def pieceLength(self) -> int:
+        """get the standard piece length for the pieces in the .torrent file"""
+        pieceLength = self.infoDict[b"piece length"] #self.infoDict already represents the outer key of ["info"] so we just access the piece length from there
+        return pieceLength
+    
+    @property
+    def pieceHashes(self) -> list[bytes]: 
+        """get a list of hashes representing the hash of each piece in the .torrent file"""
+        allHashes = self.infoDict[b"pieces"] #a continuous string of hashes that needs to be split up in order to get the hashes for individual pieces
+        arrayOfIndividualHashes = []
+        for i in range(0, len(allHashes), 20):
+            arrayOfIndividualHashes.append(allHashes[i: i+20])#append to array the individual hash
+        return arrayOfIndividualHashes
